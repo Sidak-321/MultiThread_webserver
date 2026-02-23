33530e6ac8f49d79f32e44d9828029ee68a2de37
Sidak-321 <sidaksaluja52@gmail.com>
2026-02-23 10:55:09 +0000

    Multithread without threadpool

--- server.cpp
+++ server.cpp
@@ -5,6 +5,49 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sstream>
+#include <thread>
+#include <sstream>
+
+void handle_client(int client_fd)
+{
+    char buffer[4096]={0};
+    read(client_fd,buffer,sizeof(buffer));
+
+    std::string request(buffer);
+
+    size_t pos = request.find("\r\n");
+    std::string request_line = request.substr(0, pos);
+     
+    std::istringstream iss(request_line);
+    std::string method, path, version;
+    iss >> method >> path >> version;
+
+    std::string body;
+     std::cout<<path<<method<<"\n";
+    if(path=="/")
+    {
+        body="<h1>welcome to my server</h1>";
+    }
+    else if(path=="/about")
+    {
+        body="<h1>this is a simple http server implemented in c++</h1>";
+    }
+    else
+    {
+        body="<h1>404 not found</h1>";
+    }
+    std::string response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n" 
+        "Content-Length: " + std::to_string(body.size()) + "\r\n"
+        "Connection: close\r\n"
+        "\r\n" + body;
+
+        send(client_fd, response.c_str(), response.size(), 0);
+        close(client_fd);
+
+
+}
 
 int main() {
     // intialise socket
@@ -38,64 +81,16 @@ int main() {
     std::cout << "Server listening on port 8080...\n";
    
     // accept and handle incoming connections
-    while (true) {
+     while(true)
+     {
         socklen_t addrlen = sizeof(address);
         int client_fd = accept(server_fd, (sockaddr*)&address, &addrlen);
-
         if (client_fd < 0) {
             std::cerr << "Failed to accept connection\n";
-            continue;
+            continue;   
         }
-
-        std::cout << "Client connected\n";
-        // read data from the client
-         char buffer[4096]={0};
-         ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer));
-         if (bytes_read < 0) {
-             std::cerr << "Failed to read from client\n";
-             close(client_fd);
-             continue;  
-         }
-         std:: string request(buffer);
-
-         size_t pos = request.find("\r\n");
-        std::string request_line = request.substr(0, pos);
-         
-        std::cout<<"Request Line: " << request_line << std::endl;
-        
-        std::istringstream iss(request_line);
-        std::string method, path, version;
-        iss >> method >> path >> version;
-        std::cout << "Method: " << method << ", Path: " << path << ", Version: " << version << std::endl;
-
-        //routing
-
-        std :: string body;
-        if(path=="/")
-        {
-            body="<h1>welcome to my server</h1>";
-        }
-        else if(path=="/about")
-        {
-            body="<h1>this is a simple http server implemented in c++</h1>";
-        }
-        else
-        {
-            body="<h1>404 not found</h1>";
-        }
-        // response 
-        std :: string response =
-           "HTTP/1.1 200 OK\r\n"
-              "Content-Type: text/html\r\n" 
-                "Content-Length: " + std::to_string(body.size()) + "\r\n"
-                "Connection: close\r\n"
-                "\r\n" + body;
-
-        // send response to the client
-        send(client_fd, response.c_str(), response.size(), 0);
-      
-    }
-
+        std::thread(handle_client, client_fd).detach();
+     }
 
     return 0;
 }
